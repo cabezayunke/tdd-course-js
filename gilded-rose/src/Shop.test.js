440ece83211c81e1f2c59fad24dc0ef3c43df65f
joserra <joserra@genially.com>
2022-12-20 11:18:10 +0100

    wip: add more tests

--- gilded-rose/src/Shop.test.js
+++ gilded-rose/src/Shop.test.js
@@ -2,7 +2,7 @@ import Shop from "./Shop";
 import Item from "./Item";
 
 describe('Gilded rose shop', () => {
-  test("canary test", () => {
+  test("random item should decrease quality by 1 after 1 day", () => {
     const item = new Item("foo", 1, 3);
     const shop = new Shop([item]);
 
@@ -10,4 +10,22 @@ describe('Gilded rose shop', () => {
 
     expect(item.quality).toEqual(2);
   });
+
+  test('should decrease quality by 2 once the item has expired', () => { 
+    const item = new Item("foo", 0, 3);
+    const shop = new Shop([item]);
+
+    shop.updateQuality();
+
+    expect(item.quality).toEqual(1);
+  })
+
+  test('should never decrease quality below 0', () => { 
+    const item = new Item("foo", 0, 0);
+    const shop = new Shop([item]);
+
+    shop.updateQuality();
+
+    expect(item.quality).toEqual(0);
+   })
 });
